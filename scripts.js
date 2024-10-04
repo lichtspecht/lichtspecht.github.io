33773024c92ca762d019b813d486de4e1ee56b58
Jakob <jakob.forstmann@mailbox.org>
2024-10-04 15:11:40 +0200

    fix touch handling

--- scripts.js
+++ scripts.js
@@ -1,53 +1,56 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Tab-Navigation
-  const tabs = document.querySelectorAll(".tab");
-  const tabContents = document.querySelectorAll(".tab-content");
-
-  tabs.forEach((tab) => {
-	tab.addEventListener("click", () => {
-	  document.querySelector(".tab.active").classList.remove("active");
-	  tab.classList.add("active");
-	  const tabId = tab.getAttribute("data-tab");
-	  tabContents.forEach((content) => {
-		content.classList.remove("active");
-		if (content.id === tabId) {
-		  content.classList.add("active");
-		}
-	  });
-	});
-  });
-
-
-  // Produkte dynamisch laden
-  const productGrid = document.getElementById("product-grid");
-  const newProductGrid = document.getElementById("new-product-grid");
-  
-  products.forEach((product, index) => {
-	product.imgIndex = 0;
-	const productTile = document.createElement("a");
-	productTile.classList.add("product");
-	productTile.href = product.link;
-	productTile.target="_blank"
-	productTile.innerHTML = `
-			<div class="images">
-				<img id="Image${index}" src="products/${product.name}/${product.images[product.imgIndex]}" alt="${product.name}">
-				<div class="previous" onclick="previousImage(event, ${index})"><i class="fa-solid fa-chevron-left"></i></div>
-				<div class="next" onclick="nextImage(event, ${index})"><i class="fa-solid fa-chevron-right"></i></div>
-			</div>
-			<div class="line">
-				<div class="title">${product.title}</div>
-				<div class="price">${product.price.toFixed(2)} €</div>
-			</div>
-			<div class="text">${product.text}</div>
-			<div class="icon"><i class="fa-solid fa-arrow-right"></i></div>
-		`;
-	productGrid.appendChild(productTile);
 	
-	if(index < 3)
-	{
-		newProductGrid.appendChild(productTile.cloneNode(true));
-	}
-  });
+	// Tab-Navigation
+	const tabs = document.querySelectorAll(".tab");
+	const tabContents = document.querySelectorAll(".tab-content");
+
+	tabs.forEach((tab) => {
+		tab.addEventListener("click", (event) => {
+			console.log(event);
+			document.querySelector(".tab.active").classList.remove("active");
+			tab.classList.add("active");
+			const tabId = tab.getAttribute("data-tab");
+			tabContents.forEach((content) => {
+				content.classList.remove("active");
+				if (content.id === tabId) {
+					content.classList.add("active");
+				}
+			});
+
+			closeMenu();
+		});
+	});
+
+	// Produkte dynamisch laden
+	const productGrid = document.getElementById("product-grid");
+	const newProductGrid = document.getElementById("new-product-grid");
+
+	products.forEach((product, index) => {
+		product.imgIndex = 0;
+		const productTile = document.createElement("a");
+		productTile.classList.add("product");
+		productTile.href = product.link;
+		productTile.target="_blank"
+		productTile.innerHTML = `
+				<div class="images">
+					<img id="Image${index}" src="products/${product.name}/${product.images[product.imgIndex]}" alt="${product.name}">
+					<div class="previous" onclick="previousImage(event, ${index})"><i class="fa-solid fa-chevron-left"></i></div>
+					<div class="next" onclick="nextImage(event, ${index})"><i class="fa-solid fa-chevron-right"></i></div>
+				</div>
+				<div class="line">
+					<div class="title">${product.title}</div>
+					<div class="price">${product.price.toFixed(2)} €</div>
+				</div>
+				<div class="text">${product.text}</div>
+				<div class="icon"><i class="fa-solid fa-arrow-right"></i></div>
+			`;
+		productGrid.appendChild(productTile);
+		
+		if(index < 3)
+		{
+			newProductGrid.appendChild(productTile.cloneNode(true));
+		}
+	});
 });
 
 function toggleMenu(event)
